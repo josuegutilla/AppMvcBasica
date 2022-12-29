7212a74d9c8f4909c206d69d4560d188ba53f2af
josuegutilla <105397445+josuegutilla@users.noreply.github.com>
2022-12-29 19:24:47 -0400

    Crud
    CRUD de Adicionar Fornecedor, Endereço e Produtos.

---
+++ Models/Entidade.cs
@@ -0,0 +1,12 @@
+﻿namespace AppMvcBasica.Models
+{
+    public abstract class Entidade
+    {
+        protected Entidade()
+        {
+            id = Guid.NewGuid();
+        }
+
+        public Guid id { get; set; }
+    }
+}
---
+++ Models/Fornecedor.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppMvcBasica.Models
+{
+    public class Fornecedor : Entidade
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+
+        [DisplayName("Usuário ativo?")]
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
